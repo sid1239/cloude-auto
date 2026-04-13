8bc1e295a57ccb54886250c41a60a947c9335470
Siddhant Bisht <chinubisht1971@gmail.com>
2026-04-13 08:24:19 +0530

    Update leofame_auto.py

--- leofame_auto.py
+++ leofame_auto.py
@@ -28,7 +28,7 @@ URLS = [
     "https://leofame.com/free-instagram-shares",
 ]
 
-INSTAGRAM_LINK = "https://www.instagram.com/reel/DWwKjHqkkAm/?igsh=djUzOHNvbWxlYzVs"
+INSTAGRAM_LINK = ""
 
 TELEGRAM_BOT_TOKEN = "8793923431:AAH5eX0CGpos4v6u1XEMO8LTLxPm-QcH3rA"
 TELEGRAM_CHAT_ID = "1814769108"
